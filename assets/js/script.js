9ff192a7022dbfa08ef97ff974664a7a964d0588
Felipe Joglar <felipejoglar@gmail.com>
2024-02-19 12:21:40 +0100

    Add new site

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+import * as code from './modules/code.js';
+import * as video from './modules/video.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  code.ready();
+
+  video.ready();
+
+});
